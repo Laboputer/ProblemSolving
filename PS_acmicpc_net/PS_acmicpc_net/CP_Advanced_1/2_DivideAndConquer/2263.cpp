37626eef2ca60c2a3e1bd6366bde1fa6712f4af9
hyunjini <leporem91@gmail.com>
2017-10-15 23:49:19 +0900

    improve TimeComplexity : O(N^2) ?? -> O(N)

--- PS_acmicpc_net/PS_acmicpc_net/CP_Advanced_1/2_DivideAndConquer/2263.cpp
+++ PS_acmicpc_net/PS_acmicpc_net/CP_Advanced_1/2_DivideAndConquer/2263.cpp
@@ -2,6 +2,7 @@
 #define MAXN 100005
 
 int inorder[MAXN];
+int idx[MAXN];
 int postorder[MAXN];
 int N;
 
@@ -9,21 +10,16 @@ void Pre(int s1, int e1, int s2, int e2)
 {
 	if (s1 > e1) return;
 
-	for (int i = s1; i <= e1; i++)
-	{
-		if (postorder[e2] != inorder[i]) continue;
-
-		printf("%d ", inorder[i]);
-		Pre(s1, i - 1, s2, s2 + (i - s1) - 1);
-		Pre(i + 1, e1, s2 + (i - s1), e2 - 1);
-		break;
-	}
+	int i = idx[postorder[e2]];
+	printf("%d ", inorder[i]);
+	Pre(s1, i - 1, s2, s2 + (i - s1) - 1);
+	Pre(i + 1, e1, s2 + (i - s1), e2 - 1);
 }
 
 int main()
 {
 	scanf("%d", &N);
-	for (int i = 0; i < N; i++) scanf("%d", &inorder[i]);
+	for (int i = 0; i < N; i++) scanf("%d", &inorder[i]), idx[inorder[i]] = i;
 	for (int i = 0; i < N; i++) scanf("%d", &postorder[i]);
 	Pre(0, N - 1, 0, N - 1);
 	return 0;
