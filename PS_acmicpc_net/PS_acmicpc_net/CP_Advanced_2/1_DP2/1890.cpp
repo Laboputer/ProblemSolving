db573feb9c407c74c48425eb70665d822aadeb29
Hyunjin-notepc <laboputer@gmail.com>
2018-06-06 14:47:36 +0900

    Improve Time Complexity (Problem: 1890, O(N^3) -> O(N^2))

--- PS_acmicpc_net/PS_acmicpc_net/CP_Advanced_2/1_DP2/1890.cpp
+++ PS_acmicpc_net/PS_acmicpc_net/CP_Advanced_2/1_DP2/1890.cpp
@@ -2,25 +2,21 @@
 #define MAXN 105
 
 int N;
-int a[MAXN][MAXN];
+long long a[MAXN][MAXN];
 long long d[MAXN][MAXN];
+
 int main()
 {
-	scanf("%d", &N); for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) scanf("%d", &a[i][j]);
-	d[0][0] = 1;
-	for (int i = 1; i < N; i++)
+	scanf("%d", &N); for (int i = 1; i <= N; i++) for (int j = 1; j <= N; j++) scanf("%lld", &a[i][j]);
+	d[1][1] = 1;
+	for (int i = 1; i <= N; i++) for (int j = 1; j <= N; j++)
 	{
-		for (int j = 0; j < i; j++) if (j + a[0][j] == i) d[0][i] += d[0][j];
-		for (int j = 0; j < i; j++) if (j + a[j][0] == i) d[i][0] += d[j][0];
+		if (i == N && j == N) break;
+		if (i + a[i][j] <= N) d[i + a[i][j]][j] += d[i][j];
+		if (j + a[i][j] <= N) d[i][j + a[i][j]] += d[i][j];
 	}
 
-	for (int i = 1; i < N; i++) for (int j = 1; j < N; j++)
-	{
-		for (int k = 0; k < i; k++) if (k + a[k][j] == i) d[i][j] += d[k][j];
-		for (int k = 0; k < j; k++) if (k + a[i][k] == j) d[i][j] += d[i][k];
-	}
-
-	printf("%lld\n", d[N - 1][N - 1]);
+	printf("%lld\n", d[N][N]);
 
 	return 0;
 }
