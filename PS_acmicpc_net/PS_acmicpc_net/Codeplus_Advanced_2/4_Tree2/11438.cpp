aa72e3e586bdd1e7c4a029a535d520fc3e94f4e1
Hyunjin-DESKTOP <laboputer@gmail.com>
2018-06-29 00:42:34 +0900

    LCA  알고리즘 (upper이용)

--- PS_acmicpc_net/PS_acmicpc_net/Codeplus_Advanced_2/4_Tree2/11438.cpp
+++ PS_acmicpc_net/PS_acmicpc_net/Codeplus_Advanced_2/4_Tree2/11438.cpp
@@ -1,91 +1,88 @@
 #include <stdio.h>
 #define MAXN 100005
-#define MAXP 20
 
 struct Node
 {
-	int to;
 	int next;
+	int head;
 };
 
-Node S[MAXN << 1];
+Node Shared[MAXN<<1];
 int Cnt;
 
 struct List
 {
 	int head = -1;
 	int sz = 0;
-
 	void push(int x)
 	{
-		S[Cnt] = { x, head };
-		head = Cnt++;
-		sz++;
+		Shared[Cnt] = { x, head };
+		head = Cnt++; sz++;
 	}
 };
 
-int N, M;
-List G[MAXN];
-int p[MAXN][MAXP];
-int d[MAXN];
+int N,M;
+List tree[MAXN];
+int in[MAXN], out[MAXN];
+int cnt = 0;
+int v[MAXN];
+int p[MAXN][20];
 
-void DFS(int x, int pre)
+void DFS(int parent, int x)
 {
-	int cur = G[x].head;
+	if (v[x]) return;
+	v[x] = 1;
+
+	in[x] = cnt++;
+	p[x][0] = parent;
+	int cur = tree[x].head;
 	while (cur != -1)
 	{
-		if (S[cur].to != pre)
-		{
-			p[S[cur].to][0] = x;
-			d[S[cur].to] = d[x] + 1;
-			DFS(S[cur].to, x);
-		}
-		cur = S[cur].next;
+		DFS(x, Shared[cur].next);
+		cur = Shared[cur].head;
 	}
+
+	out[x] = cnt++;
+}
+
+// Is a the parent of b ? 
+bool upper(int a, int b)
+{
+	if (in[a] <= in[b] && out[b] <= out[a]) return 1;
+	return 0;
 }
 
 int LCA(int a, int b)
 {
-	if (d[a] > d[b]) { int t = a; a = b; b = t; }
+	if (upper(a, b)) return a;
+	if (upper(b, a)) return b;
 
-	int x = d[b] - d[a];
-	for (int i = MAXP - 1; i >= 0; i--)
-		if (x & (1 << i)) b = p[b][i];
-
-	if (a == b) return a;
-
-	for (int i = MAXP - 1; i >= 0; i--)
-	{
-		if (p[a][i] != p[b][i])
-		{
+	for (int i = 19; i >= 0; i--)
+		if (!upper(p[a][i], b))
 			a = p[a][i];
-			b = p[b][i];
-		}
-	}
+
 	return p[a][0];
 }
 
 int main()
 {
 	scanf("%d", &N);
-	for (int i = 1, a, b; i < N; i++)
-	{
-		scanf("%d%d", &a, &b);
-		G[a].push(b);
-		G[b].push(a);
-	}
-	for (int i = 1; i <= N; i++) for (int j = 0; j < 20; j++) p[i][j] = -1;
-	DFS(1, 0);
+	for (int i = 1, a, b; i < N; i++) scanf("%d%d", &a, &b), tree[a].push(b), tree[b].push(a);
+	
+	for (int i = 1; i <= N; i++) v[i] = 0;
+	for (int i = 1; i <= N; i++) for (int j = 0; j < 20; j++) p[i][j] = 1;
 
-	for (int k = 1; k < MAXP; k++)
+	DFS(1, 1);
+	
+	for (int j = 1; j < 20; j++)
 		for (int i = 1; i <= N; i++)
-			if (p[i][k - 1] >= 0) p[i][k] = p[p[i][k - 1]][k - 1];
+			p[i][j] = p[p[i][j - 1]][j - 1];
 
 	scanf("%d", &M);
-	for (int i = 0, a, b; i < M; i++)
+	for (int i = 0, x, y; i < M; i++)
 	{
-		scanf("%d%d", &a, &b);
-		printf("%d\n", LCA(a, b));
+		scanf("%d%d", &x, &y);
+		printf("%d\n", LCA(x, y));
 	}
 
 	return 0;
