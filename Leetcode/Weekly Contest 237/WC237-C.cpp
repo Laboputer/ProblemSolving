f9b5363c486b9d3bd09ccda6a3a69975e8ef35a2
Hyunjin-DESKTOP <laboputer@gmail.com>
2021-04-22 16:24:00 +0900

    update wc237-C

--- Leetcode/Weekly Contest 237/WC237-C.cpp
+++ Leetcode/Weekly Contest 237/WC237-C.cpp
@@ -1,13 +1,41 @@
 class Solution {
-public:
-	int getXORSum(vector<int>& arr1, vector<int>& arr2) {
-		int sxor = 0;
-		for (int i = 0; i < arr2.size(); i++)
-			sxor ^= arr2[i];
+	struct task
+	{
+		int i;
+		int pTime;
+		bool operator < (const task& rhs) const
+		{
+			if (pTime == rhs.pTime) return i > rhs.i;
+			else return pTime > rhs.pTime;
+		}
+	};
 
-		int res = 0;
-		for (int i = 0; i < arr1.size(); i++)
-			res ^= (arr1[i] & sxor);
+public:
+	vector<int> getOrder(vector<vector<int>>& tasks) {
+		for (int i = 0; i < tasks.size(); i++) tasks[i].push_back(i);
+		sort(tasks.begin(), tasks.end());
+
+		priority_queue<task> available;
+		vector<int> res;
+		long long cT = 1, i = 0;
+		while (available.size() != 0 || i < tasks.size())
+		{
+			while (i < tasks.size() && (long long)tasks[i][0] <= cT)
+			{
+				available.push({ tasks[i][2], tasks[i][1] });
+				i++;
+			}
+            
+            if (available.size() == 0)
+            {
+                cT = tasks[i][0];
+                continue;
+            }
+
+			task top = available.top(); available.pop();
+			res.push_back(top.i);
+			cT += top.pTime;
+		}
 		return res;
 	}
 };
